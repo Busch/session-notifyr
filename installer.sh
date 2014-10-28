5da78b2e20c88c1f60294233b54ba2a1784f0cba
Busch <busch@datenschleuder.com>
2014-10-28 22:04:13 +0100

    fixed url

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get --assume-yes install curl libpam-script
-wget https://datenschleuder.com/downloads/tmp/session-notifyr_0.1-1_amd64.deb
+wget -O session-notifyr_0.1-1_amd64.deb https://github.com/Busch/session-notifyr/blob/master/session-notifyr_0.1-1_amd64.deb\?raw\=true
 dpkg -i session-notifyr_0.1-1_amd64.deb
 rm -f session-notifyr_0.1-1_amd64.deb
